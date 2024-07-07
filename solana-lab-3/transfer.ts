c0737b67a0ee25f95e2ff41fa762ca871f52f1ef
Elisa <elisa.necula@student.unitbv.ro>
2024-07-07 17:31:21 +0300

    First Commit-3 labs done

---
+++ solana-lab-3/transfer.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import {
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+  Connection,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { createMemoInstruction } from "@solana/spl-memo";
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(
+  `� Loaded our keypair securely, using an env file! Our public key is: $
+{sender.publicKey.toBase58()}`
+);
+const recipient = new PublicKey("4g8QnwiSYFAXfXwitmA3KxBU6qc2eybqinXWoV2steWz");
+
+console.log(` Attempting to send 0.01 SOL to ${recipient.toBase58()}...`);
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: sender.publicKey,
+  toPubkey: recipient,
+  lamports: 0.01 * LAMPORTS_PER_SOL,
+});
+
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  sender,
+]);
+
+console.log(`✅ Transaction confirmed, signature: ${signature}!`);
+
+const addMemoInstruction = createMemoInstruction("Hello from Solana!");
+
+transaction.add(addMemoInstruction);
+const memoText = addMemoInstruction.data.toString("utf-8");
+console.log(`� memo is ${memoText}...`);
